cd8cc953c1cce2880034473fb338c640cb899711
Domenico <mazzinidomenico@gmail.com>
2023-11-17 01:45:02 +0100

    Completati:  - calcolo codice fiscale (tramite pulsante)  - validazione del form per ogni step  - controllo sull'inserimento password
    Correzioni grafiche varie sui 3 step della registrazione

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from "react"
 import {Container, Form, Col, Row} from "react-bootstrap"
 import '../style/form.css'
 import Step from "./Step"
-import CodiceFiscale from "codice-fiscale-js";
 
 export default function SignUpForm() {
     const CodiceFiscale = require('codice-fiscale-js');
@@ -19,22 +18,70 @@ export default function SignUpForm() {
         password: '',
         confirmPassword: '',
         address: '',
-        country: '',
+        province: '',
         city: '',
         cap: '',
         phoneNumber: '',
         frontID: null,
-        backID: null
+        backID: null,
+        checkTerms: false
     })
     // TODO - Settare tutti i name dei campi del form, fedelmente a quelli di formData
+    const [btnDisabled, setBtnDisabled] = useState(true);
     const [validated, setValidated] = useState(false)
 
-    const nextStep = () => {
-        setStep(step + 1)
+    const nextStep = (e) => {
+        e.preventDefault()
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.stopPropagation()
+        }
+        setValidated(true)
+
+        switch (step) {
+            case 1:
+                if (
+                    formData.firstName &&
+                    formData.lastName &&
+                    formData.sex &&
+                    formData.birthDate &&
+                    formData.birthPlace &&
+                    formData.CF &&
+                    formData.email &&
+                    formData.password &&
+                    formData.confirmPassword) {
+                    if (formData.password === formData.confirmPassword) {
+                        setStep(step + 1)
+                    }
+                }
+                break;
+            case 2:
+                if (
+                    formData.province &&
+                    formData.city &&
+                    formData.address &&
+                    formData.phoneNumber && formData.phoneNumber.length === 10) {
+                    setStep(step + 1)
+                }
+                break;
+            case 3:
+                if (
+                    formData.frontID &&
+                    formData.backID) {
+                    setStep(step + 1)
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     const prevStep = () => {
         setStep(step - 1)
+    }
+
+    const test = () => {
+        setStep(step + 1)
     }
 
     const handleChange = async (e) => {
@@ -54,6 +101,12 @@ export default function SignUpForm() {
                 ...formData,
                 [name]: dateValue.toISOString().split('T')[0]
             })
+        } else if (type === 'checkbox') {
+            // Memorizzazione del valore booleano della checkbox
+            setFormData({
+                ...formData,
+                [name]: e.target.checked
+            })
         } else {
             // Memorizzazione di ogni altro campo testuale
             setFormData({
@@ -62,6 +115,24 @@ export default function SignUpForm() {
             })
         }
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // TODO - Gestire l'invio dei dati in blockchain
+        console.log('Dati inviati:', formData)
+        // TODO - Resettare lo stato di formData
+    }
+
+    useEffect(() => {
+        const isFormValid =
+            formData.firstName &&
+            formData.lastName &&
+            formData.birthDate &&
+            formData.sex &&
+            formData.birthPlace;
+
+        setBtnDisabled(!isFormValid);
+    }, [formData]);
 
     const computeCF = () => {
         if (
@@ -96,13 +167,6 @@ export default function SignUpForm() {
     };
 
 
-    const handleSubmit = () => {
-        // TODO - Gestire l'invio dei dati in blockchain
-        console.log('Dati inviati:', formData)
-        // TODO - Resettare lo stato di formData
-    }
-
-
     return (
         <div className="signup-form d-flex align-items-center vh-100">
             <Container fluid="md">
@@ -118,6 +182,8 @@ export default function SignUpForm() {
                                 handleChange={handleChange}
                                 handleSubmit={handleSubmit}
                                 computeCF={computeCF}
+                                btnDisabled={btnDisabled}
+                                test={test}
                             />
                         </Form>
                     </Col>
--- src/components/Step.js
+++ src/components/Step.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Form, Button, Row, Col, FloatingLabel, InputGroup} from 'react-bootstrap'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {
     faAt,
     faCalendarDays,
     faIdBadge,
     faLocationDot,
-    faLock, faMap, faPhone,
-    faVenusMars
-} from "@fortawesome/free-solid-svg-icons";
-import {Link} from "react-router-dom";
+    faLock,
+    faMap,
+    faPhone,
+    faVenusMars,
+    faEye,
+    faEyeSlash
+} from "@fortawesome/free-solid-svg-icons"
+import {Link} from "react-router-dom"
+import '../style/form.css'
+import "react-datepicker/dist/react-datepicker.css"
+import province from '../province'
 
-import "react-datepicker/dist/react-datepicker.css";
-import province from '../province';
-
-export default function Step({step, formData, nextStep, prevStep, handleChange, handleSubmit, computeCF}) {
-    const today = new Date();
-    const maxDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
+export default function Step({
+                                 step,
+                                 formData,
+                                 nextStep,
+                                 prevStep,
+                                 handleChange,
+                                 handleSubmit,
+                                 computeCF,
+                                 btnDisabled,
+                                 test
+                             }) {
+    const today = new Date()
+    const maxDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate())
+    const [showPassword, setShowPassword] = useState(false)
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false)
 
     return (
         <>
@@ -37,7 +53,6 @@ export default function Step({step, formData, nextStep, prevStep, handleChange, 
                                 />
                             </FloatingLabel>
                         </Col>
-
                         <Col className="my-1">
                             <FloatingLabel label="Cognome">
                                 <Form.Control type="text"
@@ -93,7 +108,7 @@ export default function Step({step, formData, nextStep, prevStep, handleChange, 
                         <Col className="my-1">
                             <InputGroup className="mb-2">
                                 <InputGroup.Text><FontAwesomeIcon icon={faLocationDot}/></InputGroup.Text>
-                                <FloatingLabel label="Luogo di nascita">
+                                <FloatingLabel label="Provincia di nascita">
                                     <Form.Select
                                         id="inputLuogoNascita"
                                         name="birthPlace"
@@ -108,28 +123,36 @@ export default function Step({step, formData, nextStep, prevStep, handleChange, 
                                         ))}
                                     </Form.Select>
                                 </FloatingLabel>
+                                <Form.Control.Feedback type="invalid">Inserire provincia di
+                                    nascita</Form.Control.Feedback>
                             </InputGroup>
                         </Col>
                     </Row>
                     <Row>
                         <Col xs={12} md={3} className="my-1 mb-2">
                             {/* TODO - pulsante per calcolare il codice fiscale*/}
-                            <Button variant="primary" className="w-100 h-100" onClick={computeCF}>
-                                Calcola
-                            </Button>
+                            <Button
+                                variant={btnDisabled ? "dark" : "success"}
+                                className="mb-2 h-100 w-100"
+                                onClick={computeCF}
+                                disabled={btnDisabled}
+                            >Calcola</Button>
                         </Col>
                         <Col className="my-1">
                             <InputGroup className="mb-2">
                                 <InputGroup.Text><FontAwesomeIcon icon={faIdBadge}/></InputGroup.Text>
                                 <FloatingLabel label="Codice fiscale">
-                                    <Form.Control type="text"
-                                                  id="inputCF"
-                                                  name="CF"
-                                                  placeholder="Inserire codice fiscale"
-                                                  value={formData.CF}
-                                                  autoComplete="off"
-                                                  required
-                                                  readOnly
+                                    <Form.Control
+                                        type="text"
+                                        id="inputCF"
+                                        name="CF"
+                                        placeholder="Inserire codice fiscale"
+                                        value={formData.CF === ''
+                                            ? "Inserire i dati per il calcolo del codice fiscale..."
+                                            : formData.CF}
+                                        autoComplete="off"
+                                        required
+                                        readOnly
                                     />
                                 </FloatingLabel>
                             </InputGroup>
@@ -140,14 +163,15 @@ export default function Step({step, formData, nextStep, prevStep, handleChange, 
                             <InputGroup className="mb-2">
                                 <InputGroup.Text><FontAwesomeIcon icon={faAt}/></InputGroup.Text>
                                 <FloatingLabel label="Email">
-                                    <Form.Control type="email"
-                                                  id="inputEmail"
-                                                  name="email"
-                                                  placeholder="Inserire codice fiscale"
-                                                  value={formData.email}
-                                                  onChange={handleChange}
-                                                  autoComplete="off"
-                                                  required
+                                    <Form.Control
+                                        type="email"
+                                        id="inputEmail"
+                                        name="email"
+                                        placeholder="Inserire codice fiscale"
+                                        value={formData.email}
+                                        onChange={handleChange}
+                                        autoComplete="off"
+                                        required
                                     />
                                 </FloatingLabel>
                             </InputGroup>
@@ -158,38 +182,59 @@ export default function Step({step, formData, nextStep, prevStep, handleChange, 
                             <InputGroup className="mb-2">
                                 <InputGroup.Text><FontAwesomeIcon icon={faLock}/></InputGroup.Text>
                                 <FloatingLabel label="Password">
-                                    <Form.Control type="password"
-                                                  id="inputPassword"
-                                                  name="password"
-                                                  placeholder="Inserire password"
-                                                  value={formData.password}
-                                                  onChange={handleChange}
-                                                  aria-describedby="passwordHelpBlock"
-                                                  required
+                                    <Form.Control
+                                        type={showPassword ? "text" : "password"}
+                                        id="inputPassword"
+                                        name="password"
+                                        placeholder="Inserire password"
+                                        value={formData.password}
+                                        onChange={handleChange}
+                                        aria-describedby="passwordHelpBlock"
+                                        pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]).{8,20}$"
+                                        required
                                     />
                                 </FloatingLabel>
+                                <Button
+                                    variant="light" className="eye"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                >
+                                    <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye}/>
+                                </Button>
                             </InputGroup>
                         </Col>
                         <Col className="my-1">
                             <InputGroup className="mb-2">
                                 <InputGroup.Text><FontAwesomeIcon icon={faLock}/></InputGroup.Text>
                                 <FloatingLabel label="Ripeti password">
-                                    <Form.Control type="password"
-                                                  id="inputConfirmPassword"
-                                                  name="confirmPassword"
-                                                  placeholder="Ripeti password"
-                                                  value={formData.confirmPassword}
-                                                  onChange={handleChange}
-                                                  aria-describedby="passwordHelpBlock"
-                                                  required
+                                    <Form.Control
+                                        type={showConfirmPassword ? "text" : "password"}
+                                        id="inputConfirmPassword"
+                                        name="confirmPassword"
+                                        placeholder="Ripeti password"
+                                        value={formData.confirmPassword}
+                                        onChange={handleChange}
+                                        aria-describedby="passwordHelpBlock"
+                                        pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]).{8,20}$"
+                                        required
                                     />
                                 </FloatingLabel>
+                                <Button
+                                    variant="light" className="eye"
+                                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                                >
+                                    <FontAwesomeIcon icon={showConfirmPassword ? faEyeSlash : faEye}/>
+                                </Button>
                             </InputGroup>
                         </Col>
                     </Row>
                     <Row className="justify-content-center mb-2">
-                        <Button type="submit" variant="primary" onClick={nextStep} className="mb-2">
+                        <Button type="submit" variant="primary" className="mb-2 h-100 w-100" onClick={nextStep}>
                             Avanti
+                        </Button>
+                    </Row>
+                    <Row className="justify-content-center mb-2">
+                        <Button variant="danger" className="mb-2 h-100 w-100" onClick={test}>
+                            TEST
                         </Button>
                     </Row>
                     <div>
@@ -197,6 +242,7 @@ export default function Step({step, formData, nextStep, prevStep, handleChange, 
                     </div>
                 </>
             )}
+
             {step === 2 && (
                 <>
                     <h2 align="center">Step 2</h2>
@@ -206,9 +252,10 @@ export default function Step({step, formData, nextStep, prevStep, handleChange, 
                                 <InputGroup.Text><FontAwesomeIcon icon={faLocationDot}/></InputGroup.Text>
                                 <FloatingLabel label="Provincia di residenza">
                                     <Form.Select
-                                        id="inputSesso"
-                                        name="city"
-                                        value={formData.city}
+                                        id="inputProvince"
+                                        name="province"
+                                        placeholder="Inserire provincia di residenza"
+                                        value={formData.province}
                                         onChange={handleChange}
                                         autoComplete="off"
                                         required
@@ -226,10 +273,10 @@ export default function Step({step, formData, nextStep, prevStep, handleChange, 
                                 <InputGroup.Text><FontAwesomeIcon icon={faLock}/></InputGroup.Text>
                                 <FloatingLabel label="Città di residenza">
                                     <Form.Control type="text"
-                                                  id="inputIndirizzo"
-                                                  name="address"
-                                                  placeholder="Inserire indirizzo"
-                                                  value={formData.address}
+                                                  id="inputCity"
+                                                  name="city"
+                                                  placeholder="Inserire città di residenza"
+                                                  value={formData.city}
                                                   onChange={handleChange}
                                                   autoComplete="off"
                                                   required
@@ -261,13 +308,15 @@ export default function Step({step, formData, nextStep, prevStep, handleChange, 
                             <InputGroup className="mb-2">
                                 <InputGroup.Text><FontAwesomeIcon icon={faPhone}/></InputGroup.Text>
                                 <FloatingLabel label="Numero di telefono">
-                                    <Form.Control type="text"
+                                    <Form.Control type="tel"
                                                   id="inputTelefono"
                                                   name="phoneNumber"
                                                   placeholder="Inserire numero di telefono"
                                                   value={formData.phoneNumber}
                                                   onChange={handleChange}
                                                   autoComplete="off"
+                                                  maxLength="10"
+                                                  pattern="[0-9]{10}"
                                                   required
                                     />
                                 </FloatingLabel>
@@ -286,16 +335,22 @@ export default function Step({step, formData, nextStep, prevStep, handleChange, 
                                 Avanti
                             </Button>
                         </Col>
+                        <Col xs="auto" className="my-1">
+                            <Button variant="danger" className="mb-2" onClick={test}>
+                                TEST
+                            </Button>
+                        </Col>
                     </Row>
                 </>
             )}
+
             {step === 3 && (
                 <>
                     <h2 align="center">Step 3</h2>
-                    <Row>
+                    <Row className="mb-4">
                         <Col className="my-1">
-                            <Form.Group>
-                                <Form.Label>Fronte ID</Form.Label>
+                            <Form.Group controlId="frontIdImage">
+                                <Form.Label>Fronte del documento (jpg, png, pdf)</Form.Label>
                                 <Form.Control
                                     type="file"
                                     name="frontIdImage"
@@ -306,14 +361,32 @@ export default function Step({step, formData, nextStep, prevStep, handleChange, 
                     </Row>
                     <Row>
                         <Col className="my-1">
-                            <Form.Group>
-                                <Form.Label>Retro ID</Form.Label>
+                            <Form.Group controlId="backIdImage">
+                                <Form.Label>Retro del documento (jpg, png, pdf)</Form.Label>
                                 <Form.Control
                                     type="file"
                                     name="backIdImage"
                                     onChange={handleChange}
                                 />
                             </Form.Group>
+                        </Col>
+                    </Row>
+
+                    <Row className="m-2">
+                        <Col className="d-flex justify-content-center">
+                            <Form.Check>
+                                <Form.Check.Input
+                                    type="checkbox"
+                                    id="inputCheckTerms"
+                                    name="checkTerms"
+                                    checked={formData.checkTerms}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <Form.Check.Label>
+                                    Accetto i <Link to="/terms">termini e condizioni</Link>
+                                </Form.Check.Label>
+                            </Form.Check>
                         </Col>
                     </Row>
 
@@ -329,6 +402,7 @@ export default function Step({step, formData, nextStep, prevStep, handleChange, 
                             </Button>
                         </Col>
                     </Row>
+
                 </>
             )}
         </>
