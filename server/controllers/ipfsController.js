2619fef9ef307a00c2d11f17588d5c9d1586b77d
Domenico <mazzinidomenico@gmail.com>
2023-12-07 19:08:16 +0100

    Small changes

--- server/controllers/ipfsController.js
+++ server/controllers/ipfsController.js
@@ -1,76 +1,72 @@
-import {create} from 'ipfs-http-client';
+import {create} from 'ipfs-http-client'
 
-const address = 'http://127.0.0.1:5002';
+const address = 'http://127.0.0.1:5002'
 const connect = async (req, res) => {
-    const ipfs = create(new URL("http://127.0.0.1:5002"));
-    const isOnline = ipfs.isOnline();
+    const ipfs = create(new URL("http://127.0.0.1:5002"))
+    const isOnline = ipfs.isOnline()
 
     if (isOnline) {
-        console.log("IPFS online: " + address);
-        return ipfs;
+        console.log("IPFS online: " + address)
+        return ipfs
+    } else {
+        console.log("IPFS offline")
     }
 }
 
 const saveToIpfs = async (req, res) => {
     const username = "test"
-    const ipfs = await connect();
-    const files = req.files;
-    // const userDirectoryCID = await addUser(username, ipfs);
-    const userDirectory = `/users/${username}`;
+    const ipfs = await connect()
+    const files = req.files
+    // const userDirectoryCID = await addUser(username, ipfs)
+    const userDirectory = `/users/${username}`
     try {
         for (let file of files) {
-            console.log("Nome del file: " + file.originalname);
-            console.log("Tipo MIME: " + file.mimetype);
-            console.log("Dimensione: " + file.size + "\n");
-            console.log("- - - - - - - - - - - - - - - - - - - - - - - -");
+            console.log("Nome del file: " + file.originalname)
+            console.log("Tipo MIME: " + file.mimetype)
+            console.log("Dimensione: " + file.size + "\n")
+            console.log("- - - - - - - - - - - - - - - - - - - - - - - -")
 
-            const filePath = `/users/${username}/${file.originalname}`;
-            await ipfs.files.write(filePath, file.buffer, {create: true, parents: true});
+            const filePath = `/users/${username}/${file.originalname}`
+            await ipfs.files.write(filePath, file.buffer, {create: true, parents: true})
 
-            const fileObj = await ipfs.files.stat(filePath);
-            const dirObj = await ipfs.files.stat(userDirectory);
-            console.log(`FILE: ${file.originalname} - CID: ${fileObj.cid.toString()}`);
-            console.log(`DIRECTORY: '${filePath}' - CID: ${dirObj.cid.toString()}`);
-            res.status(200).json({success: true, fileCID: fileObj.cid.toString()});
+            const fileObj = await ipfs.files.stat(filePath)
+            const dirObj = await ipfs.files.stat(userDirectory)
+            console.log(`FILE: ${file.originalname} - CID: ${fileObj.cid.toString()}`)
+            console.log(`DIRECTORY: '${filePath}' - CID: ${dirObj.cid.toString()}`)
+            res.status(200).json({success: true, fileCID: fileObj.cid.toString()})
         }
     } catch (err) {
-        console.error(err);
-        res.status(500).json({error: 'Internal Server Error'});
+        console.error(err)
+        res.status(500).json({error: 'Internal Server Error'})
     }
-};
+}
 
 const getFilesByUsername = async (req, res) => {
-    const username = req.params.username;
-    const ipfs = await connect();
-    const dirPath = `/users/${username}`;
-    const filesList = [];
+    const ipfs = await connect()
+    const username = req.params.username
+    const dirPath = `/users/${username}`
+    const filesList = []
+    let directoryExists = true;
 
     try {
-        console.log(`Richiesta elenco file per l'utente '${username}'`);
-
-        try {
-            await ipfs.files.stat(dirPath);
-        } catch (dirError) {
-            // La directory non esiste, restituisci un errore
-            console.log(`La directory '${dirPath}' non esiste.`);
-            res.status(404).json({success: false, error: 'Directory non trovata', fileList: []});
-            return;
+        console.log(`Richiesta elenco file per l'utente '${username}'`)
+        if (directoryExists) {
+            console.log(`Controllo sulla directory '${dirPath}'`)
+            for await (let file of ipfs.files.ls(dirPath)) {
+                console.log(file.name)
+                filesList.push(file)
+            }
+            console.log(`Elenco dei file nella directory '${dirPath}':`, filesList)
+            res.status(200).json({success: true, fileList: filesList})
         }
-
-        for await (let file of ipfs.files.ls(dirPath)) {
-            console.log(file.name)
-            filesList.push(file);
-        }
-        console.log(`Elenco dei file nella directory '${dirPath}':`, filesList);
-        res.status(200).json({success: true, fileList: filesList});
     } catch (err) {
-        console.error(err);
-        res.status(500).json({error: 'Internal Server Error'});
+        console.error(err)
+        res.status(500).json({error: 'Internal Server Error'})
     }
-};
+}
 
 export const ipfsController = {
     connect,
     getFilesByUsername,
     saveToIpfs,
-};
+}
--- src/pages/TestIPFS.js
+++ src/pages/TestIPFS.js
@@ -1,70 +1,88 @@
-import React, {useState} from 'react';
-import {Button, Input, List, ListItem, TextField, Typography} from '@mui/material';
-import axios from "axios";
+import React, {useState} from 'react'
+import {Button, Input, List, ListItem, TextField, Typography} from '@mui/material'
+import axios from "axios"
 
 export default function TestIPFS() {
-    const [files, setFiles] = useState([]);
-    const [uploadProgress, setUploadProgress] = useState(0);
-    const [username, setUsername] = useState('');
-    const [fileList, setFileList] = useState([]);
-    const [noFiles, setNoFiles] = useState(false);
+    const [files, setFiles] = useState([])
+    const [uploadProgress, setUploadProgress] = useState(0)
+    const [username, setUsername] = useState('')
+    const [fileList, setFileList] = useState([])
+    const [noFiles, setNoFiles] = useState(false)
+    const [pressed, setPressed] = useState(false)
 
     const handleFileChange = (e) => {
-        setFiles([...e.target.files]);
-    };
+        setFiles([...e.target.files])
+    }
 
     const handleUpload = async () => {
         if (files.length === 0) {
-            console.log("Nessun file selezionato");
-            return;
+            console.log("Nessun file selezionato")
+            return
         } else {
-            const formData = new FormData();
+            const formData = new FormData()
             files.forEach((file, index) => {
-                formData.append('file', file);
-            });
-
-            console.log("LOG: file presi in input")
+                formData.append('file', file)
+            })
 
             await axios.post('http://localhost:3001/api/ipfs/upload', formData, {
                     onUploadProgress: (progressEvent) => {
-                        const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
-                        setUploadProgress(progress);
+                        const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100)
+                        setUploadProgress(progress)
                     },
                 },
                 {
                     'Content-Type': 'multipart/form-data'
                 })
                 .then(res => {
-                    console.log(res.statusText);
-                    setUploadProgress(0);
+                    setUploadProgress(0)
                 })
                 .catch(error => {
-                    console.error(error);
-                    setUploadProgress(0);
-                });
+                    console.error(error)
+                    setUploadProgress(0)
+                })
         }
-    };
+    }
 
     const handleInputChange = (e) => {
-        setUsername(e.target.value);
-    };
+        setUsername(e.target.value)
+    }
     const handleReadFiles = async () => {
-        try {
-            const response = await axios.get(`http://localhost:3001/api/ipfs/getFilesByUsername/${username}`)
-                .then(res => {
-                    console.log(res.statusText);
+        if (username === '') {
+            console.log("Nessun username inserito")
+            return
+        } else {
+            setPressed(true)
+            try {
+                const response = await axios.get(`http://localhost:3001/api/ipfs/getFilesByUsername/${username}`)
+                console.log(response.statusText);
+                console.log("Numero dei file letti: " + response.data.fileList.length);
+
+                if (response.data.fileList.length === 0) {
+                    console.log("Nessun file presente per l'utente inserito");
+                    setNoFiles(true);
+                    setPressed(false);
+                    setFileList([]);
+                } else {
                     setNoFiles(false);
                     setFileList(response.data.fileList);
-                })
-                .catch(error => {
-                    console.error(error);
-                    setNoFiles(true);
-                    setFileList(response.data.fileList);
-                })
-        } catch (error) {
-            console.error(error);
+                }
+                // .then(res => {
+                //     console.log(res.statusText)
+                //     console.log("Numero dei file letti: " + res.data.fileList.length)
+                //     if (res.data.fileList.length > 0) {
+                //         setNoFiles(false)
+                //         setFileList(res.data.fileList)
+                //     } else {
+                //         console.log("Nessun file presente per l'utente inserito")
+                //         setNoFiles(true)
+                //         setPressed(false)
+                //     }
+                // })
+            } catch (err) {
+                console.error("Errore")
+            }
         }
-    };
+    }
 
     return (
         <>
@@ -91,25 +109,29 @@ export default function TestIPFS() {
                     Ottieni Elenco File
                 </Button>
 
-                {noFiles ?
-                    <Typography variant="h6" color="error" gutterBottom style={{marginTop: '20px'}}>
-                        Nessun file presente per l'utente inserito!
-                    </Typography>
-                    :
-                    (<>
-                            <Typography variant="h6" gutterBottom style={{marginTop: '20px'}}>
-                                Elenco dei file:
+                {
+                    pressed &&
+                    (noFiles ?
+                            <Typography variant="h6" color="error" gutterBottom style={{marginTop: '20px'}}>
+                                Nessun file presente per l'utente inserito!
                             </Typography>
-                            <List>
-                                {fileList.map((file, index) => (
-                                    <ListItem key={index}>
-                                        <Typography>{file.name}</Typography>
-                                    </ListItem>
-                                ))}
-                            </List>
-                        </>
-                    )}
+                            :
+                            (<>
+                                    <Typography variant="h6" gutterBottom style={{marginTop: '20px'}}>
+                                        Elenco dei file:
+                                    </Typography>
+                                    <List>
+                                        {fileList.map((file, index) => (
+                                            <ListItem key={index}>
+                                                <Typography>{file.name}</Typography>
+                                            </ListItem>
+                                        ))}
+                                    </List>
+                                </>
+                            )
+                    )
+                }
             </div>
         </>
-    );
-};
+    )
+}
